d2b5bbbce753fd10ff7c85e38605519d982a73b4
Daniel Geymayer <dag0310@gmail.com>
2015-02-07 21:55:57 +0100

    Add right click directive and bind it to the same event as hm-hold (e.g. editing the comment of a product)

--- src/app/app.js
+++ src/app/app.js
@@ -84,4 +84,16 @@ angular.module('shopper', [
         return params;
     };
 })
+.directive('ngRightClick', function($parse) {
+    return function(scope, element, attrs) {
+        var fn = $parse(attrs.ngRightClick);
+        element.bind('contextmenu', function(event) {
+            scope.$apply(function() {
+                event.preventDefault();
+                fn(scope, {$event:event});
+            });
+        });
+    };
+})
+;
 ;
